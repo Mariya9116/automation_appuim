7dca9efee87d78e27b76c6e3b3dfbd4a660875f6
HP <mariyaa@bluetag.no>
2023-05-28 13:31:44 +0530

    update

--- src/test/java/openApp.java
+++ src/test/java/openApp.java
@@ -12,7 +12,7 @@ public class openApp {
 
     private AndroidDriver driver;
     @Test
-    public void setUpInstiGo() throws MalformedURLException {
+    public void setUpInstiGo() throws MalformedURLException, InterruptedException {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability("appium:deviceName", "Android SDK built for x86");
         desiredCapabilities.setCapability("appium:platformVersion", "11.0");
@@ -27,24 +27,27 @@ public class openApp {
         URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
 
         driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+        Thread.sleep(10000);
+        MobileElement SkipButton = (MobileElement) driver.findElementById("com.iitdh.sonusourav.instigo:id/btn_skip");
+        SkipButton.click();
     }
-    @Test
-    public void setUpTestClock() throws MalformedURLException {
-        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        desiredCapabilities.setCapability("appium:deviceName", "Android SDK built for x86");
-        desiredCapabilities.setCapability("appium:platformVersion", "11.0");
-        desiredCapabilities.setCapability("platformName", "Android");
-        desiredCapabilities.setCapability("appium:appPackage", "com.android.deskclock");
-        desiredCapabilities.setCapability("appium:appActivity", "com.android.deskclock.DeskClock");
-        desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
-        desiredCapabilities.setCapability("appium:nativeWebScreenshot", true);
-        desiredCapabilities.setCapability("appium:newCommandTimeout", 3600);
-        desiredCapabilities.setCapability("appium:connectHardwareKeyboard", true);
-        URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
-
-        driver = new AndroidDriver(remoteUrl, desiredCapabilities);
-
-    }
+//    @Test
+//    public void setUpTestClock() throws MalformedURLException {
+//        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+//        desiredCapabilities.setCapability("appium:deviceName", "Android SDK built for x86");
+//        desiredCapabilities.setCapability("appium:platformVersion", "11.0");
+//        desiredCapabilities.setCapability("platformName", "Android");
+//        desiredCapabilities.setCapability("appium:appPackage", "com.android.deskclock");
+//        desiredCapabilities.setCapability("appium:appActivity", "com.android.deskclock.DeskClock");
+//        desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
+//        desiredCapabilities.setCapability("appium:nativeWebScreenshot", true);
+//        desiredCapabilities.setCapability("appium:newCommandTimeout", 3600);
+//        desiredCapabilities.setCapability("appium:connectHardwareKeyboard", true);
+//        URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
+//
+//        driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+//
+//    }
 
     @AfterMethod(alwaysRun = true)
     public void anotationTest5()
