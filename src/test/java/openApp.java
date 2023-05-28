a552afbcea488f76072404e773a167555760c1f7
Mariya9116 <64100966+Mariya9116@users.noreply.github.com>
2023-05-28 12:04:14 +0530

    Add files via upload
    initial commit

---
+++ src/test/java/openApp.java
@@ -0,0 +1,34 @@
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+public class openApp {
+
+    private AndroidDriver driver;
+    @Test
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("appium:deviceName", "Android SDK built for x86");
+        desiredCapabilities.setCapability("appium:platformVersion", "11.0");
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("appium:appPackage", "com.iitdh.sonusourav.instigo");
+        desiredCapabilities.setCapability("appium:appActivity", "com.iitdh.sonusourav.instigo.Account.SplashActivity");
+        desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
+        desiredCapabilities.setCapability("appium:nativeWebScreenshot", true);
+        desiredCapabilities.setCapability("appium:newCommandTimeout", 3600);
+        desiredCapabilities.setCapability("appium:connectHardwareKeyboard", true);
+
+        URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
+
+        driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+        System.out.println("Test is start");
+    }
+
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
