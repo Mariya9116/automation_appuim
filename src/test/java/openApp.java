e276fd5b2eb884a1e1455e6db8a217416071da6c
HP <mariyaa@bluetag.no>
2023-06-03 22:01:26 +0530

    ImplicitWait,ExplicitWait,FluentWait

--- src/test/java/openApp.java
+++ src/test/java/openApp.java
@@ -2,7 +2,18 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -10,7 +21,7 @@ import org.testng.annotations.Test;
 
 public class openApp {
 
-    private AndroidDriver driver;
+    private static AndroidDriver driver;
     @Test
     public void setUpInstiGo() throws MalformedURLException, InterruptedException {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
@@ -57,6 +68,49 @@ public class openApp {
 
     public void tearDown() {
         driver.quit();
+    }
+
+//    public void implicitWaitExample() {
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        driver.get("https://www.example.com");
+//        WebElement element = driver.findElement(By.id("elementId"));
+//        element.click();
+//    }
+
+    public void ImplicitWait() {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+//    public void explicitWaitExample() {
+//        driver.get("https://www.example.com");
+//        WebDriverWait wait = new WebDriverWait(driver, 10);
+//        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("elementId")));
+//        element.click();
+//    }
+
+    public WebElement ExplicitWait(WebElement ele, Duration Time) {
+        WebDriverWait wait = new WebDriverWait(driver, Time.toSeconds());
+        WebElement element = wait.until(ExpectedConditions.visibilityOf(ele));
+        return element;
+    }
+
+//    public void fluentWaitExample() {
+//        driver.get("https://www.example.com");
+//        Wait<WebDriver> wait = new FluentWait<>(driver)
+//                .withTimeout(Duration.ofSeconds(10))
+//                .pollingEvery(Duration.ofSeconds(2))
+//                .ignoring(NoSuchElementException.class);
+//        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("elementId")));
+//        element.click();
+//    }
+
+    public static WebElement FluentWait(WebElement ele, Duration Timeout, Duration Time) {
+        FluentWait<AndroidDriver> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(Timeout.toSeconds()))
+                .pollingEvery(Duration.ofSeconds(Time.toSeconds()))
+                .ignoring(NoSuchElementException.class);
+        WebElement element = wait.until(ExpectedConditions.visibilityOf(ele));
+        return element;
     }
 
 }
