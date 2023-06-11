1bb148b5fac3d6e09e721b6281584736470c74b4
HP <mariyaa@bluetag.no>
2023-06-11 11:28:47 +0530

    Commit Verify Complaint

---
+++ src/test/java/OpenAPIDemos.java
@@ -0,0 +1,85 @@
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Before;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.*;
+import org.testng.asserts.SoftAssert;
+
+
+public class OpenAPIDemos {
+
+    private AndroidDriver driver;
+
+    @BeforeMethod
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("appium:deviceName", "AOSP on IA Emulator");
+        desiredCapabilities.setCapability("appium:platformVersion", "9");
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("appium:appPackage", "io.appium.android.apis");
+        desiredCapabilities.setCapability("appium:appActivity", "io.appium.android.apis.ApiDemos");
+        desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
+        desiredCapabilities.setCapability("appium:nativeWebScreenshot", true);
+        desiredCapabilities.setCapability("appium:newCommandTimeout", 3600);
+        desiredCapabilities.setCapability("appium:connectHardwareKeyboard", true);
+
+        URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
+
+        driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    //@Test
+    public void verifyAPIDemosItems() {
+
+        SoftAssert softAssert = new SoftAssert();
+
+        MobileElement btnAccessibility = (MobileElement) driver.findElementByAccessibilityId("Accessibility");
+        softAssert.assertEquals(btnAccessibility.getText(), "Accessibility", "Accessibility is not visible");
+
+        MobileElement btnAnimation = (MobileElement) driver.findElementByAccessibilityId("Animation");
+        softAssert.assertEquals(btnAnimation.getText(), "Animation", "Animation is not visible");
+
+        MobileElement btnContent = (MobileElement) driver.findElementByAccessibilityId("Content");
+        softAssert.assertEquals(btnContent.getText(), "Content1", "Content is not visible");
+
+        MobileElement btnApp = (MobileElement) driver.findElementByAccessibilityId("App");
+        softAssert.assertEquals(btnApp.getText(), "App", "App is not visible");
+
+        MobileElement btnGraphics = (MobileElement) driver.findElementByAccessibilityId("Graphics");
+        softAssert.assertEquals(btnGraphics.getText(), "Graphics", "Graphics is not visible");
+
+        softAssert.assertAll();
+
+    }
+
+    @DataProvider(name = "apiDemosTestData")
+    public Object[][] provideTestData() {
+        return new Object[][]{
+                {"Accessibility"},
+                {"Animation"},
+                {"Content"},
+                {"App"},
+                {"Graphics"},
+
+        };
+    }
+    @Test(dataProvider = "apiDemosTestData")
+        public void verifyAPIDemosItems(String accessibilityText) {
+            SoftAssert softAssert = new SoftAssert();
+
+            MobileElement btnAccessibility = (MobileElement) driver.findElementByAccessibilityId(accessibilityText);
+            String ActualResult = btnAccessibility.getText();
+            softAssert.assertEquals(ActualResult, accessibilityText, accessibilityText + " is not visible");
+            softAssert.assertAll();
+    }
+    @AfterMethod
+    public void closeAPIDemos() {
+        driver.quit();
+    }
+}
--- src/test/java/openApp.java
+++ src/test/java/openApp.java
@@ -3,30 +3,31 @@ import io.appium.java_client.android.AndroidDriver;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
+import org.testng.asserts.SoftAssert;
 
 public class openApp {
 
     private static AndroidDriver driver;
-    @Test
+    private WebDriverWait wait;
+    @BeforeClass
     public void setUpInstiGo() throws MalformedURLException, InterruptedException {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        desiredCapabilities.setCapability("appium:deviceName", "Android SDK built for x86");
-        desiredCapabilities.setCapability("appium:platformVersion", "11.0");
+        desiredCapabilities.setCapability("appium:deviceName", "AOSP on IA Emulator");
+        desiredCapabilities.setCapability("appium:platformVersion", "9");
         desiredCapabilities.setCapability("platformName", "Android");
         desiredCapabilities.setCapability("appium:appPackage", "com.iitdh.sonusourav.instigo");
         desiredCapabilities.setCapability("appium:appActivity", "com.iitdh.sonusourav.instigo.Account.SplashActivity");
@@ -38,27 +39,182 @@ public class openApp {
         URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
 
         driver = new AndroidDriver(remoteUrl, desiredCapabilities);
-        Thread.sleep(10000);
-        MobileElement SkipButton = (MobileElement) driver.findElementById("com.iitdh.sonusourav.instigo:id/btn_skip");
-        SkipButton.click();
+
+//        ImplicitWait
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        ExplicitWait
+//        wait = new WebDriverWait(driver, 10);
+//        By btnSkip = By.id("com.iitdh.sonusourav.instigo:id/btn_skip");
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(btnSkip));
+//        driver.findElement(btnSkip).click();
+
+        //Click on Skip button
+        MobileElement btnSkip = (MobileElement) driver.findElementById("com.iitdh.sonusourav.instigo:id/btn_skip");
+        btnSkip.click();
     }
-//    @Test
-//    public void setUpTestClock() throws MalformedURLException {
-//        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-//        desiredCapabilities.setCapability("appium:deviceName", "Android SDK built for x86");
-//        desiredCapabilities.setCapability("appium:platformVersion", "11.0");
-//        desiredCapabilities.setCapability("platformName", "Android");
-//        desiredCapabilities.setCapability("appium:appPackage", "com.android.deskclock");
-//        desiredCapabilities.setCapability("appium:appActivity", "com.android.deskclock.DeskClock");
-//        desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
-//        desiredCapabilities.setCapability("appium:nativeWebScreenshot", true);
-//        desiredCapabilities.setCapability("appium:newCommandTimeout", 3600);
-//        desiredCapabilities.setCapability("appium:connectHardwareKeyboard", true);
-//        URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
-//
-//        driver = new AndroidDriver(remoteUrl, desiredCapabilities);
-//
-//    }
+    @BeforeMethod
+    public void validateInstiGoLogin() throws MalformedURLException {
+        SoftAssert softAssert = new SoftAssert();
+        //Verify the Navigate InstiGo Login page
+
+        //Enter Username or Email
+        String UserEmailAddress = "mariya1234@mailinator.com";
+        String UserPassword = "mariya1234";
+        MobileElement txtUsername = (MobileElement) driver.findElementById("com.iitdh.sonusourav.instigo:id/ed_user_name");
+        txtUsername.click();
+        txtUsername.clear();
+        txtUsername.sendKeys(UserEmailAddress);
+
+        //Enter Password
+        MobileElement txtPassword = (MobileElement) driver.findElementById("com.iitdh.sonusourav.instigo:id/ed_password");
+        softAssert.assertEquals(txtUsername, UserEmailAddress, "Enter email is wrong");
+        txtPassword.click();
+        txtPassword.clear();
+        txtPassword.sendKeys(UserPassword);
+
+        //Hide Keyboard
+        driver.hideKeyboard();
+
+        //Tab in remember me check box
+        MobileElement chkboxRememberMe = (MobileElement) driver.findElementById("com.iitdh.sonusourav.instigo:id/remember_me_chk_box");
+        chkboxRememberMe.click();
+
+        //Tab on login button
+        softAssert.assertEquals(txtPassword, UserPassword, "Enter password is wrong");
+        MobileElement btnLogin = (MobileElement) driver.findElementById("com.iitdh.sonusourav.instigo:id/button_login");
+        btnLogin.click();
+
+        //Wait ExplicitWait until Hamburger button appear
+        wait = new WebDriverWait(driver, 15);
+        By btnnHamburger = By.id("com.iitdh.sonusourav.instigo:id/imagebt_hr");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(btnnHamburger));
+
+        //Verify the landing page contents are available
+        List <MobileElement> lblInstiGo = (List<MobileElement>) driver.findElementsByClassName("android.widget.TextView");
+        lblInstiGo.get(0).getText().equalsIgnoreCase("InstiGo");
+        //String text0 = lblInstiGo.get(0).getText();
+        //String text1 = lblInstiGo.get(1).getText();
+        softAssert.assertEquals(lblInstiGo.get(0).getText(), "InstiGo", "Landing page header text is wrong");
+
+        //Tab on hamburger menu
+        MobileElement btnHamburger = (MobileElement) driver.findElementByAccessibilityId("Drawer Open");
+        btnHamburger.click();
+
+        //Verify the Email Address contents is available
+        //Wait ExplicitWait until btnStatus button appear
+//        wait = new WebDriverWait(driver, 15);
+//        By lblEmailAddressLocator = By.id("com.iitdh.sonusourav.instigo:id/complain_username");
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(lblEmailAddressLocator));
+//        MobileElement lblEmailAddress = (MobileElement) driver.findElement(lblEmailAddressLocator);
+//        lblEmailAddress.getText().equalsIgnoreCase(UserEmailAddress);
+//        System.out.println(lblEmailAddress.getText());
+//        softAssert.assertEquals(lblEmailAddress, UserEmailAddress, "Email ID is wrong");
+//        softAssert.assertAll();
+
+//        //Click on Home
+        MobileElement btnHome = (MobileElement) driver.findElementById("com.iitdh.sonusourav.instigo:id/nav_home");
+        btnHome.click();
+
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+
+    }
+    @Test(priority = 1)
+    public void verifyRegisterComplaints() throws MalformedURLException {
+        SoftAssert softAssert = new SoftAssert();
+
+        //Click on Complaints button
+//        MobileElement btnComplaints = (MobileElement) driver.findElementById("com.iitdh.sonusourav.instigo:id/nav_complaint");
+//        btnComplaints.click();
+
+        MobileElement btnComplaints = (MobileElement) driver.findElementById("com.iitdh.sonusourav.instigo:id/imagebt_bp");
+        btnComplaints.click();
+
+        //Click on Register Complaint button
+        MobileElement btnRegisterComplaint = (MobileElement) driver.findElementById("com.iitdh.sonusourav.instigo:id/maintenance_register");
+        btnRegisterComplaint.click();
+
+        //Wait ExplicitWait until txtName input filed appear
+        wait = new WebDriverWait(driver, 15);
+        By txtNameLocator = By.id("com.iitdh.sonusourav.instigo:id/complain_username");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(txtNameLocator));
+
+        //Fill ADD COMPLAINT
+        //Enter Name
+        MobileElement txtName = (MobileElement) driver.findElementById("com.iitdh.sonusourav.instigo:id/complain_username");
+        txtName.click();
+        txtName.clear();
+        txtName.sendKeys("Mariya");
+
+        //Enter Complain Title
+        MobileElement txtComplainTitle = (MobileElement) driver.findElementById("com.iitdh.sonusourav.instigo:id/complain_title");
+        txtComplainTitle.click();
+        txtComplainTitle.clear();
+        txtComplainTitle.sendKeys("Mariya_ComplainTitle");
+
+        //Enter House no.
+        MobileElement txtHouseNo = (MobileElement) driver.findElementById("com.iitdh.sonusourav.instigo:id/complain_house_no");
+        txtHouseNo.click();
+        txtHouseNo.clear();
+        txtHouseNo.sendKeys("25");
+
+        //Hide Keyboard
+        driver.hideKeyboard();
+
+        //Click on HostelListView Drop Down
+        MobileElement ddHostelListView = (MobileElement) driver.findElementById("com.iitdh.sonusourav.instigo:id/add_complaint_hostel");
+        ddHostelListView.click();
+
+        //Select HostelType
+        MobileElement lblHostelType = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.TextView[2]");
+        lblHostelType.click();
+
+        //Click on ComplaintType Drop Down
+        MobileElement ddComplaintType = (MobileElement) driver.findElementById("com.iitdh.sonusourav.instigo:id/add_complaint_type");
+        ddComplaintType.click();
+
+        //Select ComplaintType
+        MobileElement lblComplaintType = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.TextView[2]");
+        lblComplaintType.click();
+
+        //Enter ComplaintDescription
+        MobileElement txtComplaintDescription = (MobileElement) driver.findElementById("com.iitdh.sonusourav.instigo:id/et_complain_desc");
+        txtComplaintDescription.sendKeys("Complaint_Description_Test");
+
+        //Tab in Make it private me check box
+        MobileElement chkboxMakeItPrivate = (MobileElement) driver.findElementById("com.iitdh.sonusourav.instigo:id/complain_is_private");
+        chkboxMakeItPrivate.click();
+
+        //Click on submit button
+        MobileElement btnSubmit = (MobileElement) driver.findElementById("com.iitdh.sonusourav.instigo:id/complain_submit");
+        btnSubmit.click();
+
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        //Click on Back
+//        MobileElement btnBack = (MobileElement) driver.findElementByAccessibilityId("Navigate up");
+//        btnBack.click();
+
+    }
+
+    @Test(priority = 2)
+    public void setUpCheckComplaintStatus() throws MalformedURLException {
+        SoftAssert softAssert = new SoftAssert();
+
+        //Wait ExplicitWait until Status button appear
+        wait = new WebDriverWait(driver, 15);
+        By btnStatusLocator = By.id("com.iitdh.sonusourav.instigo:id/maintenance_status");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(btnStatusLocator));
+
+        //Click on Status button
+        MobileElement btnStatus = (MobileElement) driver.findElement(btnStatusLocator);
+        btnStatus.click();
+
+        //Verify the Complaint Name contents are available
+        MobileElement txtComplaintName = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.ListView/android.widget.RelativeLayout[2]/android.widget.TextView[3]");
+        softAssert.assertEquals(txtComplaintName.getText(), "Mariya_ComplainTitle", "Mariya_ComplainTitle Complain is not registered");
+    }
+
 
     @AfterMethod(alwaysRun = true)
     public void anotationTest5()
@@ -68,49 +224,6 @@ public class openApp {
 
     public void tearDown() {
         driver.quit();
-    }
-
-//    public void implicitWaitExample() {
-//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//        driver.get("https://www.example.com");
-//        WebElement element = driver.findElement(By.id("elementId"));
-//        element.click();
-//    }
-
-    public void ImplicitWait() {
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    }
-
-//    public void explicitWaitExample() {
-//        driver.get("https://www.example.com");
-//        WebDriverWait wait = new WebDriverWait(driver, 10);
-//        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("elementId")));
-//        element.click();
-//    }
-
-    public WebElement ExplicitWait(WebElement ele, Duration Time) {
-        WebDriverWait wait = new WebDriverWait(driver, Time.toSeconds());
-        WebElement element = wait.until(ExpectedConditions.visibilityOf(ele));
-        return element;
-    }
-
-//    public void fluentWaitExample() {
-//        driver.get("https://www.example.com");
-//        Wait<WebDriver> wait = new FluentWait<>(driver)
-//                .withTimeout(Duration.ofSeconds(10))
-//                .pollingEvery(Duration.ofSeconds(2))
-//                .ignoring(NoSuchElementException.class);
-//        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("elementId")));
-//        element.click();
-//    }
-
-    public static WebElement FluentWait(WebElement ele, Duration Timeout, Duration Time) {
-        FluentWait<AndroidDriver> wait = new FluentWait<>(driver)
-                .withTimeout(Duration.ofSeconds(Timeout.toSeconds()))
-                .pollingEvery(Duration.ofSeconds(Time.toSeconds()))
-                .ignoring(NoSuchElementException.class);
-        WebElement element = wait.until(ExpectedConditions.visibilityOf(ele));
-        return element;
     }
 
 }
